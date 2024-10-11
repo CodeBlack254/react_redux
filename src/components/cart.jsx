a9790a0883675b6dbe8d8cad913e2afa120408f2
CodeBlack254 <deriqrose@gmail.com>
2024-10-12 01:02:31 +0300

    added state persistence

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,17 +1,46 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
-    const addedProducts = useSelector(state => state.cart);
-    return (
-        <>
-            <h2> Cart </h2>
-            <h2> {JSON.stringify(addedProducts)} </h2>
-            <div className="m-4 grid grid-cols-3 gap-4 sm:grid-cols-12">
-                {/* <div className="min-h-[100px] rounded-lg bg-orange-500 shadow sm:col-span-1 hidden"></div> */}
-                <div className="min-h-[80vh] min-w-[85vw] rounded-lg bg-teal-500 shadow "></div>
-                {/* <div className="min-h-[100px] rounded-lg bg-purple-500 shadow sm:col-span-1 hidden"></div> */}
+    const cartProducts = useSelector(state => state.cart.items);
+
+    const dispatch = useDispatch();
+    const removeFromCart = (product) => {
+        dispatch(remove({ id: product.id }));
+    };
+
+    const cards = cartProducts.map(product => (
+        <div key={product.id} className="max-w-xs rounded overflow-hidden shadow-lg m-4 bg-white transition-transform transform hover:scale-105">
+            <img className="w-full h-100 object-cover" src={product.image} alt={product.title} />
+            <div className="px-4 py-3">
+                <div className="text-gray-600 font-bold text-lg mb-1 ">{product.title}</div>
+                <p className="text-gray-900 font-bold text-md">${product.price}</p>
+                <p className="text-gray-500 text-xs">Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
             </div>
-        </>
+            <div className="px-4 pt-3 pb-2">
+                <button onClick={() => removeFromCart(product)}
+                    className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-300 transition duration-200 ease-in-out">
+                    Remove
+                </button>
+            </div>
+        </div>
+    ))
+
+    if (cartProducts.length === 0) {
+        return (
+            <div className="max-w-xs rounded overflow-hidden shadow-lg m-4 bg-white text-center p-4">
+                <h2 className="text-gray-600 font-bold text-lg mb-2">No products found in the cart</h2>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1 className="text-xl font-bold mb-4 mt-4">My Cart</h1>
+            <div className="grid gap-4 m-4 sm:grid-cols-3 md:grid-cols-4 grid-cols-2">
+                {cards}
+            </div>
+        </div>
     )
 }
 
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,13 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
 import cartSlice from './cartSlice';
+import productSlice from './productSlice';
 
-const store = configureStore({
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedCartReducer = persistReducer(persistConfig, cartSlice);
+const persistedProductReducer = persistReducer(persistConfig, productSlice); 
+
+export const store = configureStore({
     reducer: {
-        cart: cartSlice
-        // product: productSlice
-    }
+        cart: persistedCartReducer,
+        products: persistedProductReducer, 
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+            },
+        }),
 });
 
 console.log('Initial State:', store.getState());
 
-export default store;
+export const persistor = persistStore(store);
+
+
+
